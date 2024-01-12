f33f6dcba1ad4e4e0e50d73b219403a34c1aa7fc
Yirsis Serrano <yirsishertz02@gmail.com>
2024-01-12 11:48:08 -0600

    add prettier and eslint and husky and karma config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: false,
+    },
+  },
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {},
+  settings: {},
+};
