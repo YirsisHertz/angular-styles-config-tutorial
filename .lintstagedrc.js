3bebf27b87dcc2e3038dc646603ac4e6b6c6baa8
Yirsis Serrano <yirsishertz02@gmail.com>
2024-01-12 12:30:37 -0600

    feat: validate test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "*.{ts,html}": ["eslint --fix"],
-  "*.{ts,html,json,css,scss,md}": ["prettier --write"],
+  "*.{ts,html}": ["eslint . --fix"],
+  "*.{ts,html,json,css,scss,md}": ["prettier . --write"]
 };
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe("AppComponent", () => {
   it(`should have the 'rules' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("rules!");
+    expect(app.title).toEqual("rules");
   });
 
   it("should render title", () => {
